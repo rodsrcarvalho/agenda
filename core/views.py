913480c062aba12af627695a080ee1be0de99d5f
rodsrcarvalho <rodsrcarvalho@gmail.com>
2022-06-19 16:15:34 -0300

    INCLUIDO TEMPLATE PARA INSERIR NOVO EVENTO DE AGENDA INDIVIDUALIZADA POR USUÁRIO

--- core/views.py
+++ core/views.py
@@ -35,3 +35,24 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        local = request.POST.get('local')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              local=local,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
+
+#    return render(request, 'evento.html')
